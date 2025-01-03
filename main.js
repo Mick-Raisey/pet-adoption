cb036172cb49498b2441f7982a25ea8cdfcab5b4
Mick.Raisey <tabs4bass@gmail.com>
2025-01-03 21:27:56 +1030

    Finished dynamic temperature feature

---
+++ main.js
@@ -0,0 +1,11 @@
+async function start() {
+  const weatherPromise = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  );
+  const weatherData = await weatherPromise.json();
+
+  const ourTemperatue = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperatureOutput").textContent = ourTemperatue;
+}
+
+start();
